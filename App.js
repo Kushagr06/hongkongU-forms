73933547998c46e52878d77214b2105ef1ff733c
Kushagr Narayan <75263274+Kushagr06@users.noreply.github.com>
2022-12-19 19:58:27 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,26 @@
+
+import './App.css';
+import { Component } from 'react';
+import Main from './components/Maincomponent';
+import {BrowserRouter} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
+
+const store = ConfigureStore();
+
+class App extends Component {
+
+  render(){
+  return (
+    <Provider store={store}>
+        <BrowserRouter>
+          <div className="App">
+            <Main />
+          </div>
+        </BrowserRouter>
+      </Provider>
+  );
+}
+}
+
+export default App;
---
+++ src/components/HeaderComponent.js
@@ -0,0 +1,128 @@
+import React, { Component } from 'react';
+import {Navbar, NavbarBrand,Nav,NavbarToggler, Collapse,NavItem,Button,Modal,ModalHeader,ModalBody,Label,Input,Form,FormGroup} from 'reactstrap';
+import { NavLink } from 'react-router-dom';
+import { elementor } from 'fontawesome';
+
+export class Header extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      isNavOpen: false,
+      isModalOpen: false
+  };
+  
+
+    this.toggleNav=this.toggleNav.bind(this)
+    this.toggleModal = this.toggleModal.bind(this);
+    this.handleLogin = this.handleLogin.bind(this);
+
+  }
+
+
+  
+  toggleNav(){
+    this.setState({
+      isNavOpen: !this.state.isNavOpen
+    })
+  }
+
+  toggleModal() {
+    this.setState({
+      isModalOpen: !this.state.isModalOpen
+    });
+  }
+
+  handleLogin(event){
+    this.toggleModal();
+    alert("Username:"+this.username.value)
+    event.preventDefault()
+  }
+
+  render() {
+    return (
+      <>
+      <Navbar dark expand="md">
+        <div className='container'>
+          <NavbarToggler onClick={this.state.isNavOpen} />
+          <NavbarBrand className="mr-auto" href='/'>
+            Restraunt
+          </NavbarBrand>
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav navbar>
+                <NavItem>
+                    <NavLink className="nav-link" to ="/home">
+                      <span className='fa fa-home fa-lg'></span> Home
+                    </NavLink>
+                </NavItem>
+
+                <NavItem>
+                    <NavLink className="nav-link" to ="/aboutus">
+                      <span className='fa fa-info fa-lg'></span> About Us
+                    </NavLink>
+                </NavItem>   
+
+                <NavItem>
+                    <NavLink className="nav-link" to ="/menu">
+                      <span className='fa fa-list fa-lg'></span> Menu
+                    </NavLink>
+                </NavItem>
+
+                <NavItem>
+                    <NavLink className="nav-link" to ="/contactus">
+                      <span className='fa fa-address-card fa-lg'></span> Contact Us
+                    </NavLink>
+                </NavItem>
+            </Nav>
+
+            <Nav className="ml-auto" navbar>
+                                <NavItem>
+                                    <Button outline onClick={this.toggleModal}><span className="fa fa-sign-in fa-lg"></span> Login</Button>
+                                </NavItem>
+                            </Nav>
+            </Collapse>
+        </div>
+      </Navbar>
+      <div className='container'>
+          <div className="container">
+            <div className="row row-header">
+              <div className="col-12 col-sm-6">
+                <h1>Restraut namenot known</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione fugiat corrupti labore dolore non! Alias ipsam atque ducimus facilis impedit, iure molestias fugiat, sint minima odit assumenda aut quos quaerat.</p>
+              </div>
+            </div>
+          </div>
+      </div>
+
+      <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                    <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
+                    <ModalBody>
+                    <Form onSubmit={this.handleLogin}>
+                            <FormGroup>
+                                <Label htmlFor="username">Username</Label>
+                                <Input type="text" id="username" name="username"
+                                    innerRef={(input) => this.username = input} />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label htmlFor="password">Password</Label>
+                                <Input type="password" id="password" name="password"
+                                    innerRef={(input) => this.password = input}  />
+                            </FormGroup>
+                            <FormGroup check>
+                                <Label check>
+                                    <Input type="checkbox" name="remember"
+                                    innerRef={(input) => this.remember = input}  />
+                                    Remember me
+                                </Label>
+                            </FormGroup>
+                            <Button type="submit" value="submit" color="primary">Login</Button>
+                        </Form>
+
+        </ModalBody>
+      </Modal>
+      </>
+    );
+  }
+}
+
+export default Header
